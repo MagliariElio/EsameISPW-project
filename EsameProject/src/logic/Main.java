1c1fae08724e78d2e73923de756c82f2ded73e69
Monello1299 <magliari.elio08@gmail.com>
2021-08-26 10:06:52 +0200

    update

---
+++ EsameProject/src/logic/Main.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class Main {
+
+	public static String concatenation(String variable, String variable2) {
+		return variable.concat(variable2);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(concatenation("Hello", "World"));
+	}
+
+}
